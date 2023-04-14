f01ce1418a92883025036ec3a5e5af1a902371c3
Notaduck <dev@guldberglab.info>
2023-04-14 22:53:21 +0200

    woops

--- apps/users/src/main.ts
+++ apps/users/src/main.ts
@@ -5,7 +5,7 @@ import { UserModel } from 'libs/prisma/prisma/src/zod';
 async function bootstrap() {
   const app = await NestFactory.create(UserModel);
   const rmqService = app.get<RmqService>(RmqService);
-  app.connectMicroservice(rmqService.getOptions('USER'));
+  app.connectMicroservice(rmqService.getOptions('USER_SERVICE'));
   await app.startAllMicroservices();
 }
 bootstrap();
